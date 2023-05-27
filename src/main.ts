c291b55748f3c17d32f41b6dbae354981e9fb3e4
Adil Peter <adilnesmith@ymail.com>
2023-05-27 20:53:27 +0500

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,8 @@ import { CorsOptions } from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // Configure CORS options
-  const corsOptions: CorsOptions = {
-    origin: '*', // Set the appropriate origin or origins here
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  };
-
   // Enable CORS using the configured options
-  app.enableCors(corsOptions);
+  app.enableCors();
 
   await app.listen(8000);
 }
